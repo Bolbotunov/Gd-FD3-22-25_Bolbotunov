206443f6782ec48f1e63ee8b5883f70b6177201c
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-01-28 10:36:28 +0300

    Удалена папка my-app

--- my-typescript/src/App.tsx
+++ my-typescript/src/App.tsx
@@ -1,10 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +24,3 @@ function App() {
 }
 
 export default App;
-
-
-

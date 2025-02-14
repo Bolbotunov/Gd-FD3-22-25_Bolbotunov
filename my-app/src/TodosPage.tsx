a5a965fae1238594134fb5b19ceb85639193ceca
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2025-02-14 12:50:35 +0300

    feat:add placeholder

--- my-app/src/TodosPage.tsx
+++ my-app/src/TodosPage.tsx
@@ -1,28 +1,47 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from 'react';
 import { actions } from "./stores/store";
+import { getTodos, getPosts, getTodoById } from "./api/jsonplaceholder";
 import { Todo } from "./todosSlice";
 import { v4 } from "uuid";
 import { todoSlice } from "./todosSlice";
+
+// type CallbackFunction = (error: Error | null, data? any | null) => void
+
+// function getTodos(callback: CallbackFunction) {
+//     fetch('https://jsonplaceholder.typicode.com/todos')
+//             .then(response => response.json())
+//             .then((json: JSONServerTodo[]) => {
+//                 callback(null, json);
+//             })
+//             .catch((error) => callback(error))
+//             return Promise
+// }
+
 
 export default function TodosPage() {
     const { filteredTodos } = useSelector((store: any) => store.todoSlice)
     const dispatch = useDispatch()
 
     useEffect(() => {
-        type JSONServerTodo = {
-            userId: number,
-            id: number,
-            title: string,
-            completed: boolean,
+        async function load() {
+            const data = await getTodos()
+            const dataPosts = await getPosts()
+            dispatch(actions.todoSlice.load(data))
         }
+        load()
+        }, [])
 
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then((json: JSONServerTodo[]) => {
-                dispatch(actions.todoSlice.load(json))
-            })
-    }, [])
+        // getTodos()
+        // .then((data) =>  dispatch(actions.todoSlice.load(data)))
+        // .catch((error) => console.error(error))
+
+                // fetch('https://jsonplaceholder.typicode.com/todos')
+        //     .then(response => response.json())
+        //     .then((json: JSONServerTodo[]) => {
+        //     })
+
+
 
     function addTodoHandler() {
         const title = prompt('create todo')
